6777e482a149ce713b43630cdf665b18a92cf282
mananjabrik <manan.jabrik@gmail.com>
2024-03-05 02:52:50 +0700

    update speed controller

--- espWebSocket/espWebSocket.ino
+++ espWebSocket/espWebSocket.ino
@@ -4,11 +4,19 @@
 #include <ESPAsyncWebServer.h>
 
 // Replace with your network credentials
-const char* ssid = "@wifiRoot";
-const char* password = "test225610!";
+const char *ssid = "@wifiRoot";
+const char *password = "test225610!";
 
 bool ledState = 0;
 const int ledPin = 2;
+String message = "";
+String status = "";
+String value = "";
+
+int outMotorCW = 27;
+int outMotorCCW = 14;
+int pwmCW = 0;
+int pwmCCW = 0;
 
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
@@ -37,17 +45,43 @@ const char index_html[] PROGMEM = R"rawliteral(
 </html>
 )rawliteral";
 
-void notifyClients() {
+void notifyClients()
+{
   ws.textAll(String(ledState));
 }
 
-void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
-  AwsFrameInfo *info = (AwsFrameInfo*)arg;
-  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len)
+{
+  AwsFrameInfo *info = (AwsFrameInfo *)arg;
+  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT)
+  {
     data[len] = 0;
-    Serial.print("Received message: ");
-    Serial.println((char*)data);
-    if (strcmp((char*)data, "toggle") == 0) {
+    message = (char *)data;
+
+    status = message.substring(0, message.indexOf("&"));
+    value = message.substring(message.indexOf("&") + 1, message.length());
+
+    Serial.print("Status: ");
+    Serial.println(status);
+
+    if (status == "speed")
+    {
+
+      int intValue = value.toInt();
+      int pwmMax = 255;
+      pwmCW = pwmMax * intValue / 100;
+      Serial.print("Speed Increase ");
+      Serial.println(pwmCW);
+    }
+
+    if (status == "steer")
+    {
+      Serial.print("change dir ");
+      Serial.println(value);
+    }
+
+    if (status == "toggle")
+    {
       ledState = !ledState;
       notifyClients();
     }
@@ -55,51 +89,62 @@ void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
 }
 
 void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,
-             void *arg, uint8_t *data, size_t len) {
-  switch (type) {
-    case WS_EVT_CONNECT:
-      Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
-      break;
-    case WS_EVT_DISCONNECT:
-      Serial.printf("WebSocket client #%u disconnected\n", client->id());
-      break;
-    case WS_EVT_DATA:
-      handleWebSocketMessage(arg, data, len);
-      break;
-    case WS_EVT_PONG:
-    case WS_EVT_ERROR:
-      break;
+             void *arg, uint8_t *data, size_t len)
+{
+  switch (type)
+  {
+  case WS_EVT_CONNECT:
+    Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+    break;
+  case WS_EVT_DISCONNECT:
+    Serial.printf("WebSocket client #%u disconnected\n", client->id());
+    break;
+  case WS_EVT_DATA:
+    handleWebSocketMessage(arg, data, len);
+    break;
+  case WS_EVT_PONG:
+  case WS_EVT_ERROR:
+    break;
   }
 }
 
-void initWebSocket() {
+void initWebSocket()
+{
   ws.onEvent(onEvent);
   server.addHandler(&ws);
 }
 
-String processor(const String& var){
+String processor(const String &var)
+{
   Serial.println(var);
-  if(var == "STATE"){
-    if (ledState){
+  if (var == "STATE")
+  {
+    if (ledState)
+    {
       return "ON";
     }
-    else{
+    else
+    {
       return "OFF";
     }
   }
   return String();
 }
 
-void setup(){
+void setup()
+{
   // Serial port for debugging purposes
   Serial.begin(115200);
 
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, LOW);
-  
+  pinMode(outMotorCW, OUTPUT);
+  pinMode(outMotorCCW, OUTPUT);
+
   // Connect to Wi-Fi
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(1000);
     Serial.println("Connecting to WiFi..");
   }
@@ -110,15 +155,17 @@ void setup(){
   initWebSocket();
 
   // Route for root / web page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/html", index_html, processor);
-  });
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send_P(200, "text/html", index_html, processor); });
 
   // Start server
   server.begin();
 }
 
-void loop() {
+void loop()
+{
   ws.cleanupClients();
   digitalWrite(ledPin, ledState);
+  digitalWrite(outMotorCW, pwmCW);
+  digitalWrite(outMotorCCW, LOW);
 }
